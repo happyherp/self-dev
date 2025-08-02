11674c9cb251736cbce48c797e5e6ef43d97da4c
Carlos Freund <carlosfreund@gmail.com>
2025-08-02 19:53:21 +0200

    SIP: Fix: Add version management via __version__ attribute
    Addresses issue #3
    
    ✅ All tests pass

--- src/sip/__init__.py
+++ src/sip/__init__.py
@@ -1,4 +1,11 @@
 """Top-level package for SIP."""
 
+import importlib.metadata
+
 __author__ = """Audrey M. Roy Greenfeld"""
 __email__ = "happyherp@users.noreply.github.com"
+
+try:
+    __version__ = importlib.metadata.version("sip")
+except importlib.metadata.PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
